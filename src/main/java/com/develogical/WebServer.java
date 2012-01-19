9d233a0936b7330c0be9d26e701cc08fd4be0d42
Jim Whitehead II <jnwhiteh@gmail.com>
2012-01-19 11:36:34 +0000

    Get port number from PORT environment variable

--- src/main/java/com/develogical/WebServer.java
+++ src/main/java/com/develogical/WebServer.java
@@ -44,7 +44,7 @@ public class WebServer extends HttpServlet {
     }
 
     public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+        Server server = new Server(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
