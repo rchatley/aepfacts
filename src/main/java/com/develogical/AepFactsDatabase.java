f90a994e2e608a927308f399e0d8afd1f53e2058
laurence <laurence@planet97.com>
2015-01-19 16:13:37 +0000

    added laurence to db

--- src/main/java/com/develogical/AepFactsDatabase.java
+++ src/main/java/com/develogical/AepFactsDatabase.java
@@ -34,7 +34,10 @@ public class AepFactsDatabase {
 
         data.put("ross",     miniBiog("I live in London, I've just celebrated my seventh wedding anniversary and I have a son who is two years old.", "https://1.gravatar.com/avatar/ffcb06accc2ec7657eb5cffc0c9e13e2?d=https%3A%2F%2Fidenticons.github.com%2F816955557a9d60a74bd3a1d934f6a372.png"));
         // student 1 add biog after this comment
+
         data.put("vinod",  miniBiog("Vinod is trying out Git!", "https://secure.gravatar.com/avatar/5d2759d44f8f5b396fdf21f13ab58d65?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"));
+        data.put("laurence",  miniBiog("Laurence likes GitHub!", "https://secure.gravatar.com/avatar/5d2759d44f8f5b396fdf21f13ab58d65?s=420&d=https://a248.e.akamai.net/assets.github.com%2Fimages%2Fgravatars%2Fgravatar-user-423.png"));
+
         // student 1 add biog above this comment
 
 
